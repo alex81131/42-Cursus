a4e5c96975ef7d58087aea23f0c0f75f982236a9
Kai-Erh Yeh <kyeh@made-f0Dr11s6.clusters.42paris.fr>
2024-06-04 13:56:13 +0200

    updated better solutions

--- Exam Rank 02/Level 3/pgcd/pgcd.c
+++ Exam Rank 02/Level 3/pgcd/pgcd.c
@@ -1,7 +1,30 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 
+void	pgcd(int a, int b)
+{
+	int i;
+	int gcd;
+
+	i = 1;
+	gcd = 0;
+	while (i < a && i < b)
+	{
+		if (a % i == 0 && b % i == 0)
+			gcd = i;
+		i++;
+	}
+	printf("%d", gcd);
+}
+
+int		main(int ac, char **av)
+{
+	if (ac == 3)
+		pgcd(atoi(av[1]), atoi(av[2]));
+	printf("\n");
+	return (0);
+}
+/*
 int	*ft_break(int n)
 {
 	int	i = 2;
@@ -74,3 +97,4 @@ int	main(int ac, char **av)
 		printf("\n");
 	return (0);
 }
+*/
