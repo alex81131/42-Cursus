1dd6677d03504280b71957df4c1ce0f7d533e8b9
kyeh <kyeh@student.42.fr>
2024-09-22 11:41:14 +0200

    halfway preparing microshell

--- Exam Rank 04/microshell.c
+++ Exam Rank 04/microshell.c
@@ -1,66 +1,52 @@
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kyeh <kyeh@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/22 10:59:12 by kyeh              #+#    #+#             */
+/*   Updated: 2024/09/22 11:18:57 by kyeh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int err(char *str) 
+#include "unistd.h"
+#include "string.h"
+#include "sys/wait.h"
+
+int	err(char *str)
 {
-    while (*str)
-        write(2, str++, 1);
-    return 1;
+	while (*str)
+		write(2, str++, 1);
+	return (1);
 }
 
-int cd(char **argv, int i) 
+int	cd(char **argv, int i)
 {
-    if (i != 2)
-        return err("error: cd: bad arguments\n");
-    else if (chdir(argv[1]) == -1)
-        return err("error: cd: cannot change directory to "), err(argv[1]), err("\n");
-    return 0;
+	if (i != 2)
+		return (err("error: cd: bad arguments\n"));
+	else if (chdir(argv[1]))
+		return (err("error: cd: cannot change directory to"), err(argv[1]), err("\n"));
 }
 
-int exec(char **argv, char **envp, int i) 
+int	main(int argc, char **argv, char **env)
 {
-    int fd[2];
-    int status;
-    int has_pipe = argv[i] && !strcmp(argv[i], "|");
+	int	i = 0;
+	int	status = 0;
 
-    if (has_pipe && pipe(fd) == -1)
-        return err("error: fatal\n");
-
-    int pid = fork();
-    if (!pid) 
-    {
-        argv[i] = 0;
-        if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-            return err("error: fatal\n");
-        execve(*argv, argv, envp);
-        return err("error: cannot execute "), err(*argv), err("\n");
-    }
-
-    waitpid(pid, &status, 0);
-    if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-        return err("error: fatal\n");
-    return WIFEXITED(status) && WEXITSTATUS(status);
-}
-
-int main(int argc, char **argv, char **envp) 
-{
-    int    i = 0;
-    int    status = 0;
-
-    if (argc > 1) 
-    {
-        while (argv[i] && argv[++i]) 
-        {
-            argv += i;
-            i = 0;
-            while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
-                i++;
-            if (!strcmp(*argv, "cd"))
-                status = cd(argv, i);
-            else if (i)
-                status = exec(argv, envp, i);
-        }
-    }
-    return status;
+	if (argc > 1)
+	{
+		while (argv[i] && argv[++i])
+		{
+			argv += i;
+			i = 0;
+			while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
+				i++;
+			if (!strcmp(*argv, "cd"))
+				status = cd(argv, i);
+			else if (i)
+				status = exec(argv, env, i);
+		}
+	}
+	return (status);
 }
