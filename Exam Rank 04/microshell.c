99b3e974e44e0d462a318b0686b9ca09a489de48
kyeh <kyeh@student.42.fr>
2024-09-24 10:23:41 +0200

    Prepared Ex04

--- Exam Rank 04/microshell.c
+++ Exam Rank 04/microshell.c
@@ -6,7 +6,7 @@
 /*   By: kyeh <kyeh@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 10:59:12 by kyeh              #+#    #+#             */
-/*   Updated: 2024/09/22 11:18:57 by kyeh             ###   ########.fr       */
+/*   Updated: 2024/09/24 10:20:45 by kyeh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,32 @@ int	cd(char **argv, int i)
 {
 	if (i != 2)
 		return (err("error: cd: bad arguments\n"));
-	else if (chdir(argv[1]))
+	else if (chdir(argv[1]) == -1)
 		return (err("error: cd: cannot change directory to"), err(argv[1]), err("\n"));
+	return (0);
+}
+
+int	exe(char **argv, char **env, int i)
+{
+	int	fd[2];
+	int	status;
+	int	have_pipe = argv[i] && !strcmp(argv[i], "|");
+
+	if (have_pipe && pipe(fd) == -1)
+		return (err("err: fatal\n"));
+	int	pid = fork();
+	if (!pid)
+	{
+		argv[i] = 0;
+		if (have_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+			return (err("error: fatal\n"));
+		execve(*argv, argv, env);
+		return (err("error: cannot execute "), err(*argv), err("\n"));
+	}
+	waitpid(pid, &status, 0);
+	if (have_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+			return (err("error: fatal\n"));
+	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
 
 int	main(int argc, char **argv, char **env)
@@ -45,8 +69,19 @@ int	main(int argc, char **argv, char **env)
 			if (!strcmp(*argv, "cd"))
 				status = cd(argv, i);
 			else if (i)
-				status = exec(argv, env, i);
+				status = exe(argv, env, i);
 		}
 	}
 	return (status);
 }
+// int	have_pipe = argv[i] && !strcmp(argv[i]. "|")
+// = int	have_pipe = (argv[i] && !strcmp(argv[i]. "|"))
+// 	returns 1 if both arguments are true.
+
+// execve(*pathname, pointer to cmd+arguments, env)
+
+// W = in the wait() family
+// WIDEXITED(status):
+// 	checks if a child process terminated normally
+// WEXITSTATUS(status):
+// 	retrieves the exit status of the child process, if WIFEXITED is true.
