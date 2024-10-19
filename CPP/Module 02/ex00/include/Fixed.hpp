41cce0ffef2085c5db719314cc5a909541f606bd
kyeh <kyeh@student.42.fr>
2024-10-19 23:30:44 +0200

    Finished M02/ex00-01

---
+++ CPP/Module 02/ex00/include/Fixed.hpp
@@ -0,0 +1,39 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <string>
+
+class	Fixed
+{
+	public:
+		Fixed(void);
+		Fixed(const Fixed& other);
+		Fixed&	operator = (const Fixed& other);
+		~Fixed(void);
+
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+
+	private:
+		int					_n;
+		static const int	_fraction_bit = 8;
+};
+
+#endif
+
+// Orthodox Canonical Form (OCF) refers to the practice of implementing the Rule of Three, which includes defining:
+// 	1. a "destructor",
+// 		(define a class to represent a type of object)
+// 	2. "copy constructor",
+// 		(implement a copy constructor to specify how instances of that class should be copied)
+// 		Format:
+// 			Fixed(const Fixed& other);
+// 			"other": the other object, meaning the original that is being copied.
+// 	3. and "copy assignment operator"
+// 		This operator ensures that the current object is updated to have the same state as the other object,
+// 		handling any necessary resource management (like deep copying) to avoid issues such as memory leaks or
+// 		unintended sharing of resources.
+// 		It typically returns a reference to the current object to allow for chaining assignments.
+// for classes that manage resources.
+// This approach enhances flexibility and prevents issues related to resource management.
--- CPP/Module 02/ex00/src/main.cpp
+++ CPP/Module 02/ex00/src/main.cpp
@@ -1,14 +1,17 @@
-#include "Harl.hpp"
+#include "Fixed.hpp"
+#include <iostream>
 
-int main(int ac, char **av)
-{
-	Harl	h;
+int main( void ) {
 
-	if (ac != 2 || !av[1])
-	{
-		std::cout << "Error: incorrect input." << std::endl;
-		return 1;
-	}
-	h.complain(av[1]);
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
 	return 0;
 }
