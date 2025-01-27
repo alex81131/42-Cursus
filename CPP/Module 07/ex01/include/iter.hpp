82241c3a41557396ac7a7857a6f7f1f1ace8a5fe
Kai-Erh Yeh <kyeh@bess-f2r1s4.clusters.42paris.fr>
2025-01-27 15:30:39 +0100

    Modified C++/M07/ex01 and put the subject.pdf in the right place.

--- CPP/Module 07/ex01/include/iter.hpp
+++ CPP/Module 07/ex01/include/iter.hpp
@@ -3,34 +3,15 @@
 # include <iostream>	// cout
 # include <iomanip>		// setprecision for double
 
-template <typename T>
-void	iter(T *arr, int size, void (*f)(T))
+template <typename T, typename F>
+void	iter(T *arr, int size, F f)
 {
 	for (int i = 0; i < size; i++)
-	{
 		f(arr[i]);
-	}
 }
-
-// Need the (*f)(T &) and (*f)(const T &) version,
-// because the functions below may take T & or const T & as an argument.
-template <typename T>
-void	iter(T *arr, int size, void (*f)(T &))
-{
-	for (int i = 0; i < size; i++)
-	{
-		f(arr[i]);
-	}
-}
-
-template <typename T>
-void	iter(const T *arr, int size, void (*f)(const T &))
-{
-	for (int i = 0; i < size; i++)
-	{
-		f(arr[i]);
-	}
-}
+// No need the (*f)(T &) and (*f)(const T &) version or iter(),
+// because typename F templatizes whatever resembles a function,
+// taking T & and const T & as an argument.
 
 template <typename T>
 void	triple(T &element)
@@ -40,12 +21,6 @@ void	triple(T &element)
 
 template <typename T>
 void	print_one(T &element)
-{
-	std::cout << std::fixed <<std::setprecision(2) << element << std::endl;
-}
-
-template <typename T>
-void	print_one_const(const T &element)
 {
 	std::cout << std::fixed <<std::setprecision(2) << element << std::endl;
 }
--- CPP/Module 07/ex01/src/main.cpp
+++ CPP/Module 07/ex01/src/main.cpp
@@ -5,35 +5,35 @@ int	main() {
 	int		int_arr[5] = {1, 2, 3, 4, 5};
 	iter<int>(int_arr, 5, print_one<int>);
 	std::cout<< std::endl << "Triple:" << std::endl;
-	iter<int>(int_arr, 5, triple<int>);
-	iter<int>(int_arr, 5, print_one<int>);
+	iter(int_arr, 5, triple<int>);			// It's okay to write iter in place of iter<type>,
+	iter(int_arr, 5, print_one<int>);		// because C++ reads the arguments and understands which type to use.
 	std::cout << std::endl << "Next:" << std::endl;
-	iter<int>(int_arr, 5, print_next<int>);
+	iter(int_arr, 5, print_next<int>);
 
 	std::cout << "__________ const int test __________" << std::endl;
 	int	const	int_arr_c[5] = {12, 11, 6378, 2, 299792458};
-	iter<int>(int_arr_c, 5, print_one_const<int>);
+	iter(int_arr_c, 5, print_one<const int>);		// iter<const int>
 
 	std::cout << "____________ Float test ____________" << std::endl;
 	float	float_arr[6] = {0.11, 2.21, 3.03, 11.11, -2.04, -1.23};
-	iter<float>(float_arr, 6, print_one<float>);
+	iter(float_arr, 6, print_one<float>);			// iter<float>
 	std::cout<< std::endl << "Triple:" << std::endl;
-	iter<float>(float_arr, 6, triple<float>);
-	iter<float>(float_arr, 6, print_one<float>);
+	iter(float_arr, 6, triple<float>);
+	iter(float_arr, 6, print_one<float>);
 
 	std::cout << "____________ Char test _____________" << std::endl;
 	char	char_arr[3] = {'!', '&', '#'};
-	iter<char>(char_arr, 3, print_one<char>);
+	iter(char_arr, 3, print_one<char>);				// iter<char>
 	std::cout<< std::endl << "Triple:" << std::endl;
-	iter<char>(char_arr, 3, triple<char>);
-	iter<char>(char_arr, 3, print_one<char>);
+	iter(char_arr, 3, triple<char>);
+	iter(char_arr, 3, print_one<char>);
 	
 	std::cout << "___________ String test ____________" << std::endl;
 	std::string		str[2] = {"Eleonore", "is having so much fun in Vienna."};
-	iter<std::string>(str, 2, print_one<std::string>);
+	iter(str, 2, print_one<std::string>);			// iter<std::string>
 	
 	std::cout << "________ const string test _________" << std::endl;
 	std::string	const	str_c[2] = {"Anne", "is lost in Taiwan."};
-	iter<std::string>(str_c, 2, print_one_const<std::string>);
+	iter(str_c, 2, print_one<const std::string>);	// iter<const std::string>
 	return 0;
 }
